6fda3b0659f07dba624e74048aa159a4ce8fd54c
Pan Sun <sunpan9209@gmail.com>
2014-10-19 06:19:27 -0400

    new

--- src/request.c
+++ src/request.c
@@ -146,11 +146,11 @@ void IHaveRequest(char **haschunk_list, int size, struct sockaddr_in* from) {
     printf("Response payload has: \n");
     for (num_chunks = 0; num_chunks < size; num_chunks++) {
         uint8_t buf[SHA1_HASH_LENGTH];
-        if (haschunk_list[num_chunks] != NULL) {
+     //   if (haschunk_list[num_chunks] != NULL) {
             hex2binary(haschunk_list[num_chunks], SHA1_HASH_LENGTH * 2, buf);
             insertHash(pkt, buf);
             printf("%s\n", haschunk_list[num_chunks]);
-        }
+     //   }
     }
 
     if (spiffy_sendto(getSock(), pkt->serial, getPacketSize(pkt), 0, (struct sockaddr *)from, sizeof(*from)) > 0) {
